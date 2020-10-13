66f03c2ea7672358b46f4b09592804b5db742a38
Michael Meier <mmeier@login.crane.hcc.unl.edu>
2020-10-13 09:13:40 -0500

    merge script

--- profiling/1.16S_seq_processing/1.8_merge.R
+++ profiling/1.16S_seq_processing/1.8_merge.R
@@ -8,18 +8,10 @@ load("largedata/dadaRs.rda")
 
 
 # merge together the inferred forward and reverse sequences
-mergers <- mergePairs(dadaRs, derepRs, dadaRs, derepRs)
+mergers <- mergePairs(dadaRs, derepRs, dadaRs, derepRs, verbose=TRUE)
 save(mergers, file = "largedata/mergers.rda")
 print("LOG ==> saved mergers")
 
 
-seqtabAll <- makeSequenceTable(mergers)
-save(seqtabAll, file = "largedata/seqtabAll.rda")
-print("LOG ==> saved seqtabAll")
-
-# remove chimeric sequences by comparing each inferred sequence to the others in the table
-seqtabNoC <- removeBimeraDenovo(seqtabAll)
-save(seqtabNoC, file = "largedata/seqtabNoC.rda")
-print("LOG ==> saved seqtabNoC")
 
 
