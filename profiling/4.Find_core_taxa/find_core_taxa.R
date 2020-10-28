bf71f720250ca58721cf3ca1b25133e520b55cf8
mmeier <michael.meier@huskers.unl.edu>
2020-10-28 16:04:36 -0500

    made 150 taxa circular tree

--- profiling/4.Find_core_taxa/find_core_taxa.R
+++ profiling/4.Find_core_taxa/find_core_taxa.R
@@ -2,6 +2,7 @@ library("phyloseq")
 library("tidyverse")
 library("ggtree")
 library("ggstance")
+library("ape")
 
 
 load("cache/ps_core5t.rda")
@@ -100,10 +101,10 @@ tax_groups <- select(tax_groups_man, ASV, tax_group)
 
 taxtab <- as.data.frame(tax_table(ps_core5t))
 
+# remove species column to get tax_glom to work
+
 taxtab <- taxtab %>%
-  left_join(tax_groups) %>%
-  select(-taxa) ## remove taxa column, no longer needed
-rownames(taxtab) <- taxtab$ASV
+  relocate(Genus, .after = tax_group)
 
 tax_table(ps_core5t) <- as.matrix(taxtab)
 
@@ -111,19 +112,37 @@ save(ps_core5t, file = "cache/ps_core5t.rda")
 
 
 
-library("phyloseq")
-library("tidyverse")
-
-load("cache/ps_core5t.rda")
-
-tax_table(ps_core5t)
-
 
 ### agglomerate ASV counts for each taxonomic group
+# did this on HCC
+#load("cache/ps_core5t.rda")
+
 
 ps_glom <- tax_glom(ps_core5t, taxrank="tax_group")
-
 save(ps_glom, file = "cache/ps_glom.rda")
+
+
+# find number of unique ASVs for each taxonomic group
+
+unique_asvs <- plot_data %>%
+  group_by(tax_group) %>%
+  tally(name = "unique_ASVs") %>%
+  arrange(desc(unique_ASVs)) %>%
+  filter(unique_ASVs >= 5)
+
+
+## remove taxonomic groups with fewer than 5 ASVs
+## this will be the final table at the group level
+ps_grp <- subset_taxa(ps_glom, tax_group %in% unique_asvs$tax_group)
+save(ps_grp, file = "data/ps_grp.rda")
+
+### subset the same groups in the ASV level ps object
+## this will be the final table at the ASV level
+ps_asv <- subset_taxa(ps_core5t, tax_group %in% unique_asvs$tax_group)
+save(ps_asv, file = "data/ps_asv.rda")
+
+
+
 
 
 
@@ -131,16 +150,62 @@ save(ps_glom, file = "cache/ps_glom.rda")
 
 
 
+### find node number between archaea and nearest bacteria
+find_asvs <- data.frame(tax_table(ps_grp))
+### Archaea: Candidatus Nitrocosmicus oleophilus asv_001204	
+### Proteobacteria: Burkholderia pseudomallei asv_000031
+getMRCA(phy_tree(ps_grp), c("asv_001204", "asv_004558", "asv_000552")) ## node number 201
+
+
+### root tree
+phy_tree(ps_grp) <- ape::root.phylo(phy_tree(ps_grp), node = 201)
+
+
+#### plot circular tree with annotation ####
+
+
+unique_asvs <- unique_asvs %>%
+  arrange(tax_group)
+
+
+plot_data <- data.frame(tax_table(ps_grp)) %>%
+  arrange(tax_group) %>%
+  select(-Genus,-Species) %>%
+  rownames_to_column(var = "ASV")
+
+plot_data$unique_ASVs <- unique_asvs$unique_ASVs
 
 
 
-### agglomerate taxa at subgroup level
+mycols <- c('#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075')
+
+class <- tax_table(ps_grp)[,3]
+#class[class == "Blastocatellia_(Subgroup_4)"] <- "Blastocatellia" ### fix that long name
+
+p <- ggtree(phy_tree(ps_grp), layout = "circular",  branch.length = "none") +
+  scale_x_continuous(expand=expand_scale(0.5))
+
+p2 <-gheatmap(p, class, offset=3, width=0.1, font.size=3, colnames=F) +
+  scale_fill_manual(values = mycols)
+
+p3 <- p2 %<+% plot_data +
+  geom_tiplab2(aes(angle = angle, label=unique_ASVs), size = 2, offset = 1) +
+  geom_tiplab2(aes(angle = angle, label=tax_group), size = 2.1, offset = 7) +
+  theme(plot.margin = unit(c(2,0,2,0), "cm"), legend.margin=margin(0,0,0,0, unit = "cm"), legend.box.margin=margin(0,0,0,1, unit = "cm")) +
+  labs(fill="Class") 
+
+p3
 
 
+### save img to ppt as vector graphic
 
+library("officer")
+library("rvg")
+doc <- read_pptx()
+doc <- add_slide(doc, "Title and Content", "Office Theme")
+doc <- ph_with(doc, dml(ggobj = p3), location = ph_location_fullsize())
+print(doc, target = 'figures/find_core_taxa_tree.pptx')
 
-
-ps_glom <- tax_glom(ps_final_S, taxrank=rank_names(ps_final_S)[7])
 
 
 
