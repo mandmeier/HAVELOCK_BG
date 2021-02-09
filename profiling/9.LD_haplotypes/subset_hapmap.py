19ae61442bcf3a361925fd354590bd490b0f92f6
mmeier <michael.meier@huskers.unl.edu>
2021-02-09 13:21:22 -0600

    subset hapmap

--- profiling/9.LD_haplotypes/subset_hapmap.py
+++ profiling/9.LD_haplotypes/subset_hapmap.py
@@ -1,19 +1,27 @@
 import pandas as pd
 
-hmp_large = 'largedata/hmp321_282_agpv4_maf005_miss03.hmp.txt'
 hmp_bins = 'cache/hmp_bins.csv'
+#hmp_bins = 'temp/hmp_bins_test.csv'
+hmp_large = 'largedata/hmp321_282_agpv4_maf005_miss03.hmp.txt'
+#hmp_large = 'largedata/hmp.txt'
 hmp_out = 'cache/top_snps_hmp.txt'
 
-# read data
-df = pd.read_csv(hmp_large, sep="\t")
-df['index'] = 'chr' + df.chrom.astype(str) + '_' + df.pos.astype(str).map(lambda x: x[:-4])
-
 # get bins from file
+print('get bins from file...')
 bins = pd.read_csv(hmp_bins)
 bins['index'] = 'chr' + bins.chr.astype(str) + '_' + (bins.bin - 1 ).astype(str)
 
-top_snps_hmp = df[df['index'].isin(bins['index'])]
+top_snps_hmp = pd.DataFrame()
 
-#save to tab delimited file
+# read data
+for df in pd.read_csv(hmp_large,sep='\t', chunksize=100000):
+    df['index'] = 'chr' + df.chrom.astype(str) + '_' + df.pos.astype(str).map(lambda x: x[:-4])
+    # get top snps from bins
+    top_snps = df[df['index'].isin(bins['index'])]
+    print(f'found {top_snps.shape[0]} SNPs in {df.shape[0]} rows')
+    #print(top_snps)
+    top_snps_hmp = top_snps_hmp.append(top_snps)
+
+# save to tab delimited file
+print('save txt file...')
 top_snps_hmp.to_csv(hmp_out, sep='\t', index=False)
-
