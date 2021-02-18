b8711d3c93a26cf07e9bd522527a2d03ecd57369
mmeier <michael.meier@huskers.unl.edu>
2021-02-18 11:59:59 -0600

    nice manhattan plots for top 10 microbe traits

---
+++ profiling/8.GWAS/gwas_results_top10_traits.R
@@ -0,0 +1,484 @@
+# plot GWAS of top 10 microbial traits
+
+# bins were selected if there were at least 27 significant snps above p value 5.
+# as determined by looking at bins that map to known genes.
+
+library("tidyverse")
+
+
+## select top 10 microbial traits
+
+microbial_traits <- read_csv("data/microbial_traits.csv")
+
+top_traits <- microbial_traits %>%
+  filter(GWAS_signal == "strong") %>%
+  mutate(N_treatment = ifelse(N_treatment == "stdN", "+N", "-N")) %>%
+  rename(nitrogen = N_treatment)
+
+
+## get top 10kb bins of top 10 microbes
+
+
+load("largedata/combined_SNP_data.rda")
+
+# find bins with at least 27 sig snps
+bin_regex <- paste0(".{",4,"}$")
+
+# add bins
+plot_dat <- gwas_dat %>%
+  select(-X1) %>%
+  filter(trait %in% top_traits$trait) %>%
+  #filter(-log10(p_wald) >= 1) %>%
+  mutate(bin = as.numeric(gsub(bin_regex, '', ps)) + 1) %>%
+  group_by(chr, bin, nitrogen, trait) %>%
+  add_count(count = sum(-log10(p_wald) >= 5)) %>%
+  filter(count >= 10) %>%
+  left_join(select(top_traits, trait, tax_group, nitrogen, GWAS_signal)) %>%
+  filter(GWAS_signal == "strong") %>%
+  mutate(log10p = -log10(p_wald)) %>%
+  mutate(chrbin = paste0(chr, "_", bin))
+  
+#length(unique(plot_dat$rs))
+
+
+
+
+### manhattan plots
+
+
+
+
+
+draw_manhattan <- function(snp_dat, taxgrp, chromosomes = c(1:10), threshold_line = 5, zoom = "none") {
+  
+  #snp_dat <- read_tsv("largedata/top_10_traits/T75.assoc.txt")
+  
+  #snp_dat <- read_tsv("largedata/top_10_traits/T6.assoc.txt")
+  #taxgrp <- "Acinetobacter nosocomialis"
+  #chromosomes <- c(1:10)
+  #threshold_line <- 5
+  
+  sign_snps <- plot_dat %>%
+    filter(tax_group == taxgrp)
+  
+  title <- paste(taxgrp, "|", sign_snps$nitrogen[1])
+  
+  
+  plot_obj <- snp_dat %>%
+    select(rs, chr, ps, p_wald) %>%
+    mutate(log10p = -log10(p_wald)) %>%
+    filter(log10p >= 2) %>%
+    select(-p_wald)
+  
+  
+  chr_len <- c(306970668, 244420837, 235651057, 246967222, 223706090, 173536910, 181718119, 181046068, 159686117, 150929986)
+  #vlines <- c(0,cumsum(chr_len)) ## draw vertical lines at chomosome ends
+  
+  nCHR <- length(unique(snp_dat$chr))
+  
+  chindex <- c(0) ## this number needs to be added to position within each chromosome
+  for (i in c(1:nCHR)){
+    #print(i)
+    chindex[i+1] <- sum(chr_len[1:i])
+  }
+  chindex <- chindex[1:10]
+  bpadd <- data.frame("chr" = c(1:nCHR), "chindex" = chindex)
+
+  
+  plot_obj <- plot_obj %>%
+    left_join(bpadd, by = "chr") %>%
+    mutate(psabs = ps + chindex) # calculate absolute snp position
+  
+  
+  ## find position for labels
+  axis_set <- plot_obj %>% 
+    group_by(chr) %>% 
+    summarize(center = (max(psabs) + min(psabs)) / 2) # find middle of chromosome (to place labels)
+  
+  
+  ## subset chromosomes
+  plot_obj <- plot_obj %>%
+    filter(chr %in% chromosomes)
+  
+  if(is.list(zoom)) {
+    plot_obj <- plot_obj %>%
+      filter(chr == zoom[1]) %>%
+      filter(ps >= zoom[2] & ps <= zoom[3])
+  }
+  
+  # find bins with at least 27 sig snps
+  bin_regex <- paste0(".{",4,"}$")
+  
+  plot_obj <- plot_obj %>%
+    mutate(bin = as.numeric(gsub(bin_regex, '', ps)) + 1) %>%
+    mutate(chrbin = paste0(chr, "_", bin)) %>%
+    mutate(color_group = ifelse(chrbin %in% unique(sign_snps$chrbin), "sig", chr))
+  
+  
+  ## find range for y axis
+  ylim <- floor(max(plot_obj$log10p, na.rm = TRUE)) + 1
+  
+  
+  colors = c("1" = "#58CCED", "3" = "#58CCED", "5" = "#58CCED", "7" = "#58CCED", "9" = "#58CCED",
+    "2" = "#183059", "4" = "#183059", "6" = "#183059", "8" = "#183059", "10" = "#183059", "sig" = "#ff0000")
+  
+  if(is.list(zoom)) {
+    
+    chindex <- plot_obj$chindex[1]
+    lbl <- paste0(c(seq(unlist(zoom[2])/1000, unlist(zoom[3])/1000, 10)), "k")
+    brks <- c(seq(unlist(zoom[2])+chindex, unlist(zoom[3])+chindex, 10000))
+    lms <- c(unlist(zoom[2])+chindex, unlist(zoom[3])+chindex)
+    
+    manhplot <- ggplot(plot_obj, aes(x = psabs, y = log10p, color = color_group)) +
+      geom_point(alpha = 0.75) +
+      scale_x_continuous(label = lbl,  breaks = brks,  limits = lms) +
+      scale_y_continuous(expand = c(0,0), limits = c(2, ylim), breaks = c(2:ylim)) +
+      scale_color_manual(values = colors) +
+      scale_size_continuous(range = c(0.5,3)) +
+      labs(x = paste("chr", zoom[1]), y = "-log10(p)") + 
+      theme_bw() +
+      theme( 
+        legend.position = "none",
+        panel.border = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.text.x = element_text(angle = 90, size = 8, vjust = 0.5)
+      )
+    
+  } else {
+    
+  manhplot <- ggplot(plot_obj, aes(x = psabs, y = log10p, color = color_group)) +
+    geom_point(alpha = 0.75) +
+    #geom_hline(yintercept = -log10(5), color = "grey40", linetype = "dashed") + 
+    #scale_x_continuous(label = axis_set$chr, breaks = axis_set$center, limits = c(1273000000, 1273300000)) +
+    scale_x_continuous(label = axis_set$chr, breaks = axis_set$center) +
+    scale_y_continuous(expand = c(0,0), limits = c(2, ylim), breaks = c(2:ylim)) +
+    geom_hline(yintercept=threshold_line, linetype="dashed", color = "red") +
+    #geom_vline(xintercept = vlines) +
+    scale_color_manual(values = colors) +
+    scale_size_continuous(range = c(0.5,3)) +
+    labs(x = NULL, y = "-log10(p)") + 
+    ggtitle(title) +
+    theme_bw() +
+    theme( 
+      legend.position = "none",
+      panel.border = element_blank(),
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank()#,
+      #axis.text.x = element_text(angle = 90, size = 8, vjust = 0.5)
+    )
+  
+  }
+  
+  return(manhplot)
+  
+}
+
+library("stringr")
+
+
+plot_manh <- function(tgr){
+  #tgr <- "Acinetobacter nosocomialis"
+  trait <- filter(top_traits, tax_group == tgr)$trait
+  infile <- paste0("largedata/top_10_traits/", trait, ".assoc.txt")
+  snps <- read_tsv(infile)
+  mplot <- draw_manhattan(snps, tgr, chromosomes = c(1:10))
+  outfile <- paste0("figures/Manhattan_plots_top10/", str_replace_all(tgr," ","_"),".png")
+  ggsave(filename=outfile, plot=mplot, width = 8, height = 4)
+}
+
+
+## draw all 10 Manhattan plots
+
+for (tgr in top_traits$tax_group){
+  print(paste("plotting", tgr))
+  plot_manh(tgr)
+}
+
+
+
+
+
+### zoom in to region around SNPS
+
+
+
+
+
+
+
+plot_zoom <- function(taxgroup, snps, bins, save = FALSE){
+  
+  ## get chrom
+  chrm <- str_split(bins[1], "_")[[1]][1]
+  ## get bin positions
+  binpos <- bins %>%
+    map(function(x) str_split(x, "_")[[1]][2]) %>%
+    unlist(use.names=FALSE) %>%
+    as.numeric()
+  
+  ## get middle position
+  middle_pos <- (binpos[1]-1) + (binpos[length(binpos)]-(binpos[1]-1))/2
+  lower <- (middle_pos-10)*10000
+  upper <- (middle_pos+10)*10000
+  zoom <- list(chrm, lower, upper)
+  
+  mplot <- draw_manhattan(snps, taxgroup, zoom = zoom)
+  
+  if (save == TRUE) {
+    outfile <- paste0("figures/Manhattan_plots_top10/", str_replace_all(taxgroup," ","_"),"_chr",chrm,"_",lower,"-",upper,".png")
+    ggsave(filename=outfile, plot=mplot, width = 8, height = 2)
+  }
+
+  return(mplot)
+  
+}
+
+
+
+
+tgr <- "Acinetobacter nosocomialis"
+tgr_snps <- read_tsv("largedata/top_10_traits/T6.assoc.txt")
+tgr_top_snps <- plot_dat %>%
+  filter(tax_group == tgr)
+## bins
+tgr_top_bins <- unique(tgr_top_snps$chrbin)
+
+mplot <- plot_zoom(tgr, tgr_snps, tgr_top_bins, save = TRUE)
+mplot
+
+
+
+tgr <- "Candidatus Udaeobacter copiosus"
+tgr_snps <- read_tsv("largedata/top_10_traits/T54.assoc.txt")
+tgr_top_snps <- plot_dat %>%
+  filter(tax_group == tgr)
+## bins
+tgr_top_bins <- unique(tgr_top_snps$chrbin)
+
+mplot <- plot_zoom(tgr, tgr_snps, tgr_top_bins, save = TRUE)
+mplot
+
+
+
+tgr <- "f_A21b"
+tgr_snps <- read_tsv("largedata/top_10_traits/T128.assoc.txt")
+tgr_top_snps <- plot_dat %>%
+  filter(tax_group == tgr)
+## bins
+tgr_top_bins <- unique(tgr_top_snps$chrbin)
+
+chr3_loc1 <- c("3_2692", "3_2693")
+mplot <- plot_zoom(tgr, tgr_snps, chr3_loc1, save = TRUE)
+mplot
+
+chr3_loc2 <- c("3_17437", "3_17438", "3_17441", "3_17442")
+mplot <- plot_zoom(tgr, tgr_snps, chr3_loc2, save = TRUE)
+mplot
+
+
+chr9_loc1 <- c("9_9224", "9_9225", "9_9229", "9_9231")
+mplot <- plot_zoom(tgr, tgr_snps, chr9_loc1, save = TRUE)
+mplot
+
+chr9_loc2 <- c("9_9253", "9_9256")
+mplot <- plot_zoom(tgr, tgr_snps, chr9_loc2, save = TRUE)
+mplot
+
+chr9_loc3 <- c("9_9274", "9_9277")
+mplot <- plot_zoom(tgr, tgr_snps, chr9_loc3, save = TRUE)
+mplot
+
+chr9_loc4 <- c("9_13916", "9_13917")
+mplot <- plot_zoom(tgr, tgr_snps, chr9_loc4, save = TRUE)
+mplot
+
+
+
+tgr <- "f_Comamonadaceae Unknown Genus"
+tgr_snps <- read_tsv("largedata/top_10_traits/T145.assoc.txt")
+tgr_top_snps <- plot_dat %>%
+  filter(tax_group == tgr)
+## bins
+tgr_top_bins <- unique(tgr_top_snps$chrbin)
+
+
+chr2_loc1 <- c("2_21744", "2_21745")
+mplot <- plot_zoom(tgr, tgr_snps, chr2_loc1, save = TRUE)
+mplot
+
+chr5_loc1 <- c("5_14749", "5_14750", "5_14753", "5_14754", "5_14755", "5_14756")
+mplot <- plot_zoom(tgr, tgr_snps, chr5_loc1, save = TRUE)
+mplot
+
+
+
+
+tgr <- "Filimonas sp 2"
+tgr_snps <- read_tsv("largedata/top_10_traits/T75.assoc.txt")
+tgr_top_snps <- plot_dat %>%
+  filter(tax_group == tgr)
+## bins
+tgr_top_bins <- unique(tgr_top_snps$chrbin)
+
+
+chr1_loc1 <- c("1_1277", "1_1278", "1_1280", "1_1282", "1_1286")
+mplot <- plot_zoom(tgr, tgr_snps, chr1_loc1, save = TRUE)
+mplot
+
+chr1_loc2 <- c("1_21325", "1_21326", "1_21327", "1_21331", "1_21332", "1_21333", "1_21334", "1_21335", "1_21338", "1_21340", "1_21344")
+mplot <- plot_zoom(tgr, tgr_snps, chr1_loc2, save = TRUE)
+mplot
+
+chr4_loc1 <- c("4_16096", "4_16097")
+mplot <- plot_zoom(tgr, tgr_snps, chr4_loc1, save = TRUE)
+mplot
+
+## left some out "5_2686"  "5_2691" "5_3122"
+
+chr5_loc3 <- c("5_3579", "5_3580", "5_3585", "5_3592", "5_3600", "5_3608")
+mplot <- plot_zoom(tgr, tgr_snps, chr5_loc3, save = TRUE)
+mplot
+
+chr5_loc4 <- c("5_3757", "5_3769", "5_3771", "5_3772", "5_3773", "5_3774")
+mplot <- plot_zoom(tgr, tgr_snps, chr5_loc4, save = TRUE)
+mplot
+
+
+
+
+tgr <- "Ilumatobacter"
+tgr_snps <- read_tsv("largedata/top_10_traits/T97.assoc.txt")
+tgr_top_snps <- plot_dat %>%
+  filter(tax_group == tgr)
+## bins
+tgr_top_bins <- unique(tgr_top_snps$chrbin)
+
+
+chr2_loc1 <- c("2_10610", "2_10649")
+mplot <- plot_zoom(tgr, tgr_snps, chr2_loc1, save = TRUE)
+mplot
+
+chr5_loc1 <- c("5_8326", "5_8333")
+mplot <- plot_zoom(tgr, tgr_snps, chr5_loc1, save = TRUE)
+mplot
+
+chr5_loc2 <- c("5_8343", "5_8346")
+mplot <- plot_zoom(tgr, tgr_snps, chr5_loc2, save = TRUE)
+mplot
+
+chr5_loc3 <- c("5_8363", "5_8390")
+mplot <- plot_zoom(tgr, tgr_snps, chr5_loc3, save = TRUE)
+mplot
+
+
+
+tgr <- "Massilia niabensis"
+tgr_snps <- read_tsv("largedata/top_10_traits/T130.assoc.txt")
+tgr_top_snps <- plot_dat %>%
+  filter(tax_group == tgr)
+## bins
+tgr_top_bins <- unique(tgr_top_snps$chrbin)
+
+mplot <- plot_zoom(tgr, tgr_snps, tgr_top_bins, save = TRUE)
+mplot
+
+
+tgr <- "Niabella yanshanensis"
+tgr_snps <- read_tsv("largedata/top_10_traits/T83.assoc.txt")
+tgr_top_snps <- plot_dat %>%
+  filter(tax_group == tgr)
+## bins
+tgr_top_bins <- unique(tgr_top_snps$chrbin)
+
+
+chr8_loc1 <- c("8_11943", "8_11944")
+mplot <- plot_zoom(tgr, tgr_snps, chr8_loc1, save = TRUE)
+mplot
+
+chr8_loc2 <- c("8_11954", "8_11969")
+mplot <- plot_zoom(tgr, tgr_snps, chr8_loc2, save = TRUE)
+mplot
+
+
+
+tgr <- "Rhizobium daejeonense"
+tgr_snps <- read_tsv("largedata/top_10_traits/T34.assoc.txt")
+tgr_top_snps <- plot_dat %>%
+  filter(tax_group == tgr)
+## bins
+tgr_top_bins <- unique(tgr_top_snps$chrbin)
+
+
+chr4_loc1 <- c("4_21403", "4_21404")
+mplot <- plot_zoom(tgr, tgr_snps, chr4_loc1, save = TRUE)
+mplot
+
+chr6_loc1 <- c("6_1818", "6_1835")
+mplot <- plot_zoom(tgr, tgr_snps, chr6_loc1, save = TRUE)
+mplot
+
+chr6_loc2 <- c("6_1839", "6_1855")
+mplot <- plot_zoom(tgr, tgr_snps, chr6_loc2, save = TRUE)
+mplot
+
+chr6_loc3 <- c("6_1863", "6_1868")
+mplot <- plot_zoom(tgr, tgr_snps, chr6_loc3, save = TRUE)
+mplot
+
+## omitted chr 7 and 10 "7_10195" "7_10217" "10_6676" "10_8723"
+
+
+
+
+tgr <- "Sphingobium herbicidovorans 1"
+tgr_snps <- read_tsv("largedata/top_10_traits/T28.assoc.txt")
+tgr_top_snps <- plot_dat %>%
+  filter(tax_group == tgr)
+## bins
+tgr_top_bins <- unique(tgr_top_snps$chrbin)
+
+
+chr2_loc1 <- c("2_6979", "2_6980")
+mplot <- plot_zoom(tgr, tgr_snps, chr2_loc1, save = TRUE)
+mplot
+
+chr3_loc1 <- c("3_310", "3_311")
+mplot <- plot_zoom(tgr, tgr_snps, chr3_loc1, save = TRUE)
+mplot
+
+chr8_loc1 <- c("8_14527", "8_14530")
+mplot <- plot_zoom(tgr, tgr_snps, chr8_loc1, save = TRUE)
+mplot
+
+chr9_loc1 <- c("9_1163", "9_1164")
+mplot <- plot_zoom(tgr, tgr_snps, chr9_loc1, save = TRUE)
+mplot
+
+
+
+
+
+
+
+
+
+
+### find intervals for LD plots for all candidate loci
+
+loci <- list(
+  list(c(3, 187340, 187420)),
+  list(c(9, 103530, 103590))
+)
+
+
+names(loci) <- c("Acinetobacter nosocomialis", "Candidatus Udaeobacter copiosus")
+
+loci
+
+
+
+
+loci$`Acinetobacter nosocomialis`[[1]][2]
+
+
--- profiling/9.LD_haplotypes/LD_analysis.R
+++ profiling/9.LD_haplotypes/LD_analysis.R
@@ -4,6 +4,7 @@ library("tidyverse")
 library("genetics")
 library("LDheatmap")
 library("stringr")
+library("ggpubr")
 
 
 
@@ -32,11 +33,13 @@ trait_dat <- top_bins %>%
 
 ### get haplotype data for top bins
 
-haplotypes <- read.table("cache/top_snps_hmp.txt",head=T,com="",sep="\t",row=1,na.string="NN")
+haplotypes <- read.table("cache/top_snps_hmp_2.txt",head=T,com="",sep="\t",row=1,na.string="NN")
 haplotypes <- haplotypes %>% 
   dplyr::select(-index) %>%
   rownames_to_column(var = "snp_id")
   
+
+
 
 
 
@@ -48,10 +51,13 @@ gwas_dat <- gwas_dat %>%
   mutate(snp_id = paste0("S", chr, "_", ps))
 
 
-RhM <- gwas_dat %>%
-  filter(tax_group == "Rhizobium mesosinicum")
+#RhM <- gwas_dat %>%
+  #filter(tax_group == "Rhizobium mesosinicum")
 
+ano <- gwas_dat %>%
+  filter(tax_group == "Acinetobacter nosocomialis")
 
+ano$log10p <- -log10(ano$p_wald)
 
 
 ### function to find SNPs to plot
@@ -123,7 +129,7 @@ hapmap <- function(microbe, N_treatment, chromosome, bins, draw=TRUE) {
 
 
 
-#hapmap("Acinetobacter nosocomialis", "+N", 3, c(18736, 18741), draw=TRUE)
+hapmap("Acinetobacter nosocomialis", "+N", 3, c(18736, 18739, 18741), draw=TRUE)
 
 
 arch <- hapmap("Acinetobacter nosocomialis", "+N", 3, c(18741), draw=FALSE)
@@ -134,6 +140,8 @@ top_loci <- top_bins %>%
   unique()
 
 
+
+## make dummy table to fill in
 top_snps <- hapmap("Acinetobacter nosocomialis", "+N", 3, c(18741), draw=FALSE) %>%
   filter(log10p == max(log10p))
 
@@ -154,38 +162,158 @@ for (loc in 1:nrow(top_loci)) {
 }
 
 
-sort(unique())
 
 
 
 
+## pivot longer and fix genotype nomenclature
+
+genotype_names_all <- read_csv("data/BG_MM_Gen_names.csv")
+gnames <- genotype_names_all %>%
+  dplyr::select(MM_name, GX_name) %>%
+  rename(genotype = MM_name)
 
 
-sort(unique(gwas_dat$tax_group))
-
-
-tax <- gwas_dat %>%
-  filter(tax_group == "Acinetobacter nosocomialis") %>%
-  mutate(log10p = -log10(p_wald))
-
-
-
-
-### find most significant SNP for each GWAS peak to determine haplotypes
-
-test <- top_bins
-
-test$top_snp
-
-
-
-
-
-
-
+top_snps <- top_snps %>%
+  dplyr::select(-X1, -assembly., -protLSID, -assayLSID, -panelLSID, -QCcode, -center, -strand) %>%
+  pivot_longer(
+    cols = X33.16:Yu796,
+    names_to = "GX_name",
+    values_to = "haplotype"
+  ) %>%
+  mutate(GX_name = str_replace_all(GX_name, "[.]", "-")) %>%
+  mutate(GX_name = str_replace_all(GX_name, "^X", "")) %>%
+  mutate(GX_name = str_replace_all(GX_name, "MO1W", "Mo1W")) %>%
+  mutate(GX_name = str_replace_all(GX_name, "Ms71", "MS71")) %>%
+  mutate(GX_name = str_replace_all(GX_name, "OH7B", "Oh7B")) %>%
+  left_join(gnames) %>%
+  drop_na(genotype) 
 
 
 
 
+### get microbe counts for stdN and lowN
+load("cache/counts_per_genotype.rda")
+
+count_data_stdN <- filter(count_data, nitrogen == "+N")
+top_snps_stdN <- top_snps %>%
+  rename(GWAS_nitrogen = nitrogen) %>%
+  mutate(nitrogen = "+N") %>%
+  left_join(count_data_stdN)
+
+count_data_lowN <- filter(count_data, nitrogen == "-N")
+top_snps_lowN <- top_snps %>%
+  rename(GWAS_nitrogen = nitrogen) %>%
+  mutate(nitrogen = "-N") %>%
+  left_join(count_data_lowN)
+
+top_snps <- rbind(top_snps_stdN, top_snps_lowN)
+
+
+
+genotype_names_hap <- sort(unique(top_snps$genotype))
+
+## missing genotypes
+genotype_names_hap_GX <- sort(unique(top_snps$GX_name))
+genotype_names_hap_MM <- sort(unique(gnames$genotype))
+
+## missing 16 genotypes
+genotype_names_hap_MM[!(genotype_names_hap_MM %in% genotype_names_hap)]
+
+
+## remove heterozygous and NA
+
+string <- "AA"
+string <- "TT"
+string <- "AT"
+
+str_detect(string, "TT|AA|GG|CC")
+
+
+top_snps <- top_snps %>%
+  mutate(haplotype = ifelse(str_detect(haplotype, "TT|AA|GG|CC"), haplotype, NA)) %>%
+  drop_na(haplotype)
+
+
+#save(top_snps, file="data/top_snps_haplotypes.rda")
+
+
+
+top10_taxa <- c("Acinetobacter nosocomialis", "Candidatus Udaeobacter copiosus",
+  "f_A21b", "f_Comamonadaceae Unknown Genus", "Filimonas sp 2", "Ilumatobacter",
+  "Massilia niabensis", "Niabella yanshanensis", "Rhizobium daejeonense", "Sphingobium herbicidovorans 1")
+
+
+colors <- c("AA"="#d2e2ef", "CC"="#fcd2c2", "GG"="#dfe8d5", "TT"="#ffeabf")
+
+
+
+taxgrp <- top10_taxa[10]
+
+tax <- top_snps %>%
+  filter(tax_group %in% taxgrp)
+
+p <- ggplot(tax, aes(x=nitrogen, y=count, fill=haplotype)) +
+    #geom_boxplot() +
+    geom_boxplot(outlier.shape = NA) +
+    geom_point(pch = 21, size = 1, position = position_jitterdodge(jitter.width = 0.1)) +
+    facet_wrap(~snp_id, nrow = 1) +
+    stat_compare_means(aes(group = haplotype), label = "p.signif") +
+    ylab("ASV count") +
+    scale_fill_manual(values = colors) +
+    ggtitle(paste(taxgrp, "|", tax$GWAS_nitrogen[1])) +
+    theme_bw() +
+    theme(axis.title.x = element_blank())
+
+p
+
+#dev.off()
+
+#compare_means(count ~ haplotype, data = tax, 
+  #group.by = c("nitrogen","snp_id"))
+
+
+
+taxgrp <- top10_taxa[10]
+
+snp_pvalues <- top_snps %>%
+  dplyr::select(tax_group, GWAS_nitrogen, snp_id, log10p) %>%
+  unique() %>%
+  filter(tax_group == taxgrp)
+
+
+
+### select NAM parents
+
+
+NAM_set <- c("B73", "B97", "CML103", "CML228", "CML277", "CML322",
+  "CML333", "CML52", "CML69", "HP301", "IL14H", "KI11",
+  "M162W", "M37W", "MS71", "NC350", "NC358", "OH43",
+  "OH7B", "P39", "TZI8")
+
+
+
+
+NAM_top_snps <- top_snps %>%
+  filter(genotype %in% NAM_set)
+
+
+taxgrp <- top10_taxa[1]
+
+tax <- NAM_top_snps %>%
+  filter(tax_group %in% taxgrp)
+
+
+p <- ggplot(tax, aes(x=genotype, y=count, fill=haplotype)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  #geom_boxplot(outlier.shape = NA) +
+  #geom_point(pch = 21, size = 1, position = position_jitterdodge(jitter.width = 0.1)) +
+  facet_wrap(~nitrogen + snp_id, nrow = 2) +
+  ylab("ASV count") +
+  scale_fill_manual(values = colors) +
+  ggtitle(paste(taxgrp, "|", tax$GWAS_nitrogen[1])) +
+  theme_bw()
+
+p
 
 
